66356a500e6c6d51407f936d6501cb57a5d1f029
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-01-05 12:36:24 +0300

    Completed Array Find Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+
+const winningYear = superbowlWin(record)
+
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === "W");
+    return winningYear ? winningYear.year : undefined;
+}
